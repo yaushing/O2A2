d4b257b25ac4f35ed920f1b3bc2cc85b3a44beb4
Yau Shing <75722100+yaushing@users.noreply.github.com>
2023-01-11 18:28:03 +0800

    Stpo weather from auto loading

--- test/weather.js
+++ test/weather.js
@@ -5,7 +5,8 @@ xhr.withCredentials = true;
 
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+        let text = this.responseText;
+		console.log(text);
 	}
 });
 
